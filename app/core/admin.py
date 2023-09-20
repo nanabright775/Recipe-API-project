caf5ea42d8b3068bd7fcf5ae1bdcf61dac084738
nanabright775 <nanabrightatsiatorme@gmail.com>
2023-09-20 18:49:06 +0600

    added superuser and admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,46 @@
+"""django admin customization"""
+
 from django.contrib import admin # noqa
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+from django.utils.translation import gettext_lazy as _
 
 # Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    """define the admin pages for user"""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields':('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields':(
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (
+            None, {
+                'classes':('wide',),
+                'fields':(
+                    'email',
+                    'password1',
+                    'password2',
+                    'name',
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+    )
+
+admin.site.register(models.User, UserAdmin)
